c270a88fc6b168d81ec136a2f0e7d827a993218e
ranajaydas <ranajay@outlook.com>
2019-10-29 10:53:49 +0100

    Completed upto section 4.7

---
+++ suorganizer/organizer/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=31, unique=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config')
+
+    def __str__(self):
+        return self.name.title()
+
+    class Meta:
+        ordering = ['name']
+
+
+class Startup(models.Model):
+    name = models.CharField(max_length=31, db_index=True)
+    slug = models.SlugField(max_length=31, unique=True, help_text='A label for URL config')
+    description = models.TextField()
+    founded_date = models.DateField('date founded')
+    contact = models.EmailField()
+    website = models.URLField(max_length=255)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
+
+
+class NewsLink(models.Model):
+    title = models.CharField(max_length=63)
+    link = models.URLField(max_length=255)
+    pub_date = models.DateField('date published')
+    startup = models.ForeignKey(Startup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.startup}:{self.title}'
+
+    class Meta:
+        verbose_name = 'news article'
+        ordering = ['-pub_date']
+        get_latest_by = 'pub_date'
+
---
+++ suorganizer/organizer/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Tag
+
+
+def homepage(request):
+    tag_list = Tag.objects.all()
+    output = '<br>'.join([tag.name for tag in tag_list])
+    return HttpResponse(output)
