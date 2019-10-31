98321f5e118a297bcc22d4790710f8a671b494ea
ranajaydas <ranajay@outlook.com>
2019-10-31 10:04:26 +0100

    Completed chapter 7

---
+++ suorganizer/organizer/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Tag, NewsLink, Startup
+
+
+class SlugCleanMixin:
+    """Mixin class for slug cleaning methods."""
+
+    def clean_slug(self):
+        """Ensure all slugs are lowercase and user does not make a slug named 'create'."""
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError("Slug may not be 'create'.")
+
+        return new_slug
+
+
+class TagForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Tag
+        fields = '__all__'  # We could have also specified fields = ['name', 'slug']
+
+    def clean_name(self):
+        """Ensure all tag names are lowercase."""
+        return self.cleaned_data['name'].lower()
+
+
+class StartupForm(SlugCleanMixin, forms.ModelForm):
+    class Meta:
+        model = Startup
+        fields = '__all__'
+
+
+class NewsLinkForm(forms.ModelForm):
+    class Meta:
+        model = NewsLink
+        fields = '__all__'
