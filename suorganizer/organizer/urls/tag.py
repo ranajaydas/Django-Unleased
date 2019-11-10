6b11261ec544f48eece2e30e4a9e04196bdbb9a9
ranajaydas <ranajay@outlook.com>
2019-11-10 11:51:16 +0100

    Completed chapter 16

--- suorganizer/organizer/urls/tag.py
+++ suorganizer/organizer/urls/tag.py
@@ -4,7 +4,7 @@ from ..views import TagList, TagPageList, tag_detail, TagCreate, TagUpdate, TagD
 
 urlpatterns = [
     path('', TagList.as_view(), name='organizer_tag_list'),
-    path('<page_number>/', TagPageList.as_view(), name='organizer_tag_page'),
+    path('<int:page_number>/', TagPageList.as_view(), name='organizer_tag_page'),
     path('create/', TagCreate.as_view(), name='organizer_tag_create'),
     path('<slug>', tag_detail, name='organizer_tag_detail'),
     path('<slug>/update', TagUpdate.as_view(), name='organizer_tag_update'),
